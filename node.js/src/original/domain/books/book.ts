bb7d49badcab21e7fddd4db30b788cb306790f1b
Oskar Dudycz <oskar.dudycz@gmail.com>
2023-09-12 17:47:34 +0200

    added nesting in Domain project

--- node.js/src/original/domain/books/book.ts
+++ node.js/src/original/domain/books/book.ts
@@ -1,4 +1,4 @@
-import { Aggregate } from '../infrastructure/aggregates';
+import { Aggregate } from '../../infrastructure/aggregates';
 import { BookId } from './entities/bookId';
 
 export class Book extends Aggregate<BookId> {
