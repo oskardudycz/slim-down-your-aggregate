522b7846192650cfc47405db64bd7ffafa033b69
Oskar Dudycz <oskar.dudycz@gmail.com>
2023-09-13 11:33:42 +0200

    Implemented book factory

--- node.js/src/original/domain/books/factories/bookFactory.ts
+++ node.js/src/original/domain/books/factories/bookFactory.ts
@@ -1,23 +1,41 @@
-export interface BookFactory {
-  //   create(data: {
-  //     bookId: BookId,
-  //     state: State,
-  //     Title title,
-  //     Author author,
-  //     IPublishingHouse publishingHouse,
-  //     Publisher publisher,
-  //     PositiveInt edition,
-  //     Genre? genre,
-  //     ISBN? isbn,
-  //     DateOnly? publicationDate,
-  //     PositiveInt? totalPages,
-  //     PositiveInt? numberOfIllustrations,
-  //     NonEmptyString? bindingType,
-  //     NonEmptyString? summary,
-  //     CommitteeApproval? committeeApproval,
-  //     List<Reviewer> reviewers,
-  //     List<Chapter> chapters,
-  //     List<Translation> translations,
-  //     List<Format> formats}
-  // ): Book;
+import { List } from 'immutable';
+import { State, Book } from '../book';
+import {
+  Author,
+  BookId,
+  Chapter,
+  CommitteeApproval,
+  Format,
+  Genre,
+  ISBN,
+  Publisher,
+  Reviewer,
+  Title,
+  Translation,
+} from '../entities';
+import { NonEmptyString, PositiveNumber } from '#core/typing';
+import { IPublishingHouse } from '../services/publishingHouse';
+
+export interface IBookFactory {
+  create(
+    bookId: BookId,
+    state: State,
+    title: Title,
+    author: Author,
+    publishingHouse: IPublishingHouse,
+    publisher: Publisher,
+    edition: PositiveNumber,
+    genre: Genre | null,
+    isbn: ISBN | null,
+    publicationDate: Date | null,
+    totalPages: PositiveNumber | null,
+    numberOfIllustrations: PositiveNumber | null,
+    bindingType: NonEmptyString | null,
+    summary: NonEmptyString | null,
+    committeeApproval: CommitteeApproval | null,
+    reviewers: Reviewer[] | null,
+    chapters: Chapter[] | null,
+    translations: Translation[] | null,
+    formats: Format[] | null,
+  ): Book;
 }
