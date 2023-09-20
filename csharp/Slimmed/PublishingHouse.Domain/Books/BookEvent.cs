df2c214aa8f97ac74a274766e331a6dcf3d3c419
Oskar Dudycz <oskar.dudycz@gmail.com>
2023-09-20 13:03:44 +0200

    Added all events placing them in dedicated files (removing old ones).
    Used all the events as the outcomes of the business logic

---
+++ csharp/Slimmed/PublishingHouse.Domain/Books/BookEvent.cs
@@ -0,0 +1,75 @@
+using PublishingHouse.Books.Entities;
+using PublishingHouse.Core;
+using PublishingHouse.Core.ValueObjects;
+
+namespace PublishingHouse.Books;
+
+public abstract record BookEvent: IDomainEvent
+{
+    public record DraftCreated(
+        BookId BookId,
+        Title Title,
+        Author Author,
+        Publisher Publisher,
+        PositiveInt Edition,
+        Genre? Genre
+    ): BookEvent;
+
+    public record ChapterAdded(
+        BookId BookId,
+        Chapter Chapter
+    ): BookEvent;
+
+    public record FormatAdded(
+        BookId BookId,
+        Format Format
+    ): BookEvent;
+
+    public record FormatRemoved(
+        BookId BookId,
+        Format Format
+    ): BookEvent;
+
+    public record TranslationAdded(
+        BookId BookId,
+        Translation Translation
+    ): BookEvent;
+
+    public record ReviewerAdded(
+        BookId BookId,
+        Reviewer Reviewer
+    ): BookEvent;
+
+    public record MovedToEditing(
+        BookId BookId
+    ): BookEvent;
+
+    public record Approved(
+        BookId BookId,
+        CommitteeApproval CommitteeApproval
+    ): BookEvent;
+
+    public record ISBNSet(
+        BookId BookId,
+        ISBN ISBN
+    ): BookEvent;
+
+    public record MovedToPrinting
+    (
+        BookId BookId
+    ): BookEvent;
+
+    public record Published(
+        BookId BookId,
+        ISBN ISBN,
+        Title Title,
+        Author Author
+    ): BookEvent;
+
+    public record MovedToOutOfPrint
+    (
+        BookId BookId
+    ): BookEvent;
+
+    private BookEvent() { }
+}
--- csharp/Slimmed/PublishingHouse.Domain/Books/Events/ChapterAddedEvent.cs
+++
@@ -1,6 +0,0 @@
-using PublishingHouse.Books.Entities;
-using PublishingHouse.Core;
-
-namespace PublishingHouse.Books.Events;
-
-public record ChapterAddedEvent(BookId BookId, Chapter Chapter): IDomainEvent;
